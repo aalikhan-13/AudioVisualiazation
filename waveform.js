d5f919277e155a71773ac9d446587f5fdc7515a0
aali khan <aalisemail@gmail.com>
2025-04-24 23:08:39 -0500

    started work on a basic waveform visualizer from an uploaded audio file

---
+++ waveform.js
@@ -0,0 +1,36 @@
+const canvas = document.getElementById("waveform") // accessses visualization element from html
+const ctx = canvas.getContext('2d')
+
+canvas.width = canvas.offsetWidth
+canvas.height = canvas.offsetHeight
+
+const input = document.getElementById("audioFileInput") // accesses file input element from html
+
+let audioCtx; // main web audio controller 
+let analyzer; // audio node
+let dataArray; // holds data value
+let bufferLength; // values per frame
+
+input.addEventListener('change', async (e) => { // e is the event (user uploads a file)
+    const file = e.target.files[0]; // saves the file input as file
+    if (!file) return;
+
+    const arrayBuffer = await file.arrayBuffer(); // converts the file into raw binary decodable data (array buffer)
+    audioCtx = new AudioContext(); // creates a new audio context
+
+    const audioBuffer = await audioCtx.decodeAudioData(arrayBuffer); // decodes binary data into playable audio
+
+    const source = audioCtx.createBufferSource(); // creates a new audio source
+    source.buffer = audioBuffer; // attatches the buffer to the audio source - it can now be played back
+
+    analyzer = audioCtx.createAnalyser(); // creates analyzer node - allows us to access data
+    analyzer.fftSize = 2048; // 2048 samples per frame
+
+    bufferLength = analyzer.fftSize;
+    dataArray = new Uint8Array(bufferLength) // creates a new array to store data values betweeen 0 and 255
+
+    source.connect(analyzer);
+    analyzer.connect(audioCtx.destination); // audio travels from source -> analyzer -> destination (speakers)
+
+    
+})
