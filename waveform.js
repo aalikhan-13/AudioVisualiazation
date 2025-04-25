274bf1e8800ccd6353cce62ae1d85aa966bb85bf
aali khan <aalisemail@gmail.com>
2025-04-25 12:11:01 -0500

    finished waveform visualizer

--- waveform.js
+++ waveform.js
@@ -27,10 +27,42 @@ input.addEventListener('change', async (e) => { // e is the event (user uploads 
     analyzer.fftSize = 2048; // 2048 samples per frame
 
     bufferLength = analyzer.fftSize;
-    dataArray = new Uint8Array(bufferLength) // creates a new array to store data values betweeen 0 and 255
+    dataArray = new Uint8Array(bufferLength); // creates a new array to store data values betweeen 0 and 255
 
     source.connect(analyzer);
     analyzer.connect(audioCtx.destination); // audio travels from source -> analyzer -> destination (speakers)
 
-    
+    source.start();
+    draw();
 })
+
+function draw() {
+    requestAnimationFrame(draw);
+    analyzer.getByteTimeDomainData(dataArray); // fills data array with values
+
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, canvas.width, canvas.height); // clears the canvas
+
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = '#0f0';
+    ctx.beginPath();
+
+    const sliceWidth = canvas.width / bufferLength; // horizontal width of each point
+    let x = 0; // current canvas location
+
+    for (let i = 0; i < bufferLength; i ++) {
+        const v = dataArray[i] / 128.0; // normalizing data around the center (128)
+        const y = (v * canvas.height) / 2; // map to canvas height
+
+        if (i == 0) {
+            ctx.moveTo(x,y); // start the path
+        }
+        else {
+            ctx.lineTo(x,y); // connect the line to build the waveform
+        }
+        x += sliceWidth;
+    }
+
+    ctx.lineTo(canvas.width, canvas.height / 2); // end line at the right side
+    ctx.stroke();
+}
